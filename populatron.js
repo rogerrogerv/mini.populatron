cd35642a55e05897aa239ae86f363eaf04fca98a
Roger V <60841571+rogerrogerv@users.noreply.github.com>
2020-04-16 02:29:01 +0900

    Add neatCsv and the requirements to be able to read csv files in JS

--- populatron.js
+++ populatron.js
@@ -1,4 +1,14 @@
+const neatCsv = require("neat-csv");
 const fs = require("fs");
+
+const totalPopulation = (callback) => {
+  fs.readFile("./cities.csv", async (err, data) => {
+    if (err) {
+      throw err;
+    }
+    console.log(await neatCsv(data));
+  });
+};
 
 module.exports = {
   totalPopulation(onFinished) {},
